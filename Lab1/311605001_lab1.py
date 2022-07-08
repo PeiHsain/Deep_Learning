2e992670cf51e1d50d39e6f0c17104a4a82c8c82
TsaiPeiHsain <susan4323@gmail.com>
2022-07-08 14:55:57 +0800

    first commit

---
+++ Lab1/311605001_lab1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+2022 Summer DL lab1 created by Pei-Hsuan Tsai.
+    Implement simple neural networks with forwarding pass and backpropagation using two hidden layers.
+"""
+
+import numpy as np
+
+# Check the GPU is avialible, else use the CPU
+
+
+if __name__ == "__main__":
